412865ade5d1da36fa525c57ec99885e794eadf4
Danyil Tetertik <cyclepartsua@gmail.com>
2026-07-30 20:45:16 +0300

    feat(seo): dynamically generate sitemap based on physical file system instead of static json

--- server/utils/sitemap-generator.js
+++ server/utils/sitemap-generator.js
@@ -4,23 +4,29 @@ const path = require('path');
 const ROOT_DIR = path.join(__dirname, '..', '..');
 const TEMPLATE_PATH = path.join(ROOT_DIR, 'sitemap.template.xml');
 const OUTPUT_PATH = path.join(ROOT_DIR, 'sitemap.xml');
-const ARTICLES_DATA_PATH = path.join(ROOT_DIR, 'server', 'data', 'articles.seed.json');
+const ARTICLES_DIR = path.join(ROOT_DIR, 'articles');
+const RU_ARTICLES_DIR = path.join(ROOT_DIR, 'ru', 'articles');
 
 function generateSitemap() {
   try {
     let template = fs.readFileSync(TEMPLATE_PATH, 'utf-8');
-    let articles = [];
-    if (fs.existsSync(ARTICLES_DATA_PATH)) {
-      articles = JSON.parse(fs.readFileSync(ARTICLES_DATA_PATH, 'utf-8'));
-    }
-
     let dynamicUrls = '';
-    
-    articles.forEach(article => {
-      if (article.status === 'published') {
-        const slug = article.slug;
-        const lastmod = (article.updated_at || article.created_at || new Date().toISOString()).split('T')[0];
+    let count = 0;
+
+    if (fs.existsSync(ARTICLES_DIR)) {
+      const slugs = fs.readdirSync(ARTICLES_DIR).filter(file => {
+        return fs.statSync(path.join(ARTICLES_DIR, file)).isDirectory() && 
+               fs.existsSync(path.join(ARTICLES_DIR, file, 'index.html'));
+      });
+
+      slugs.forEach(slug => {
+        count++;
+        // Get last modified date from index.html
+        const stat = fs.statSync(path.join(ARTICLES_DIR, slug, 'index.html'));
+        const lastmod = stat.mtime.toISOString().split('T')[0];
         
+        const hasRu = fs.existsSync(path.join(RU_ARTICLES_DIR, slug, 'index.html'));
+
         dynamicUrls += `
   <url>
     <loc>https://endo.kh.ua/articles/${slug}</loc>
@@ -29,14 +35,13 @@ function generateSitemap() {
     <priority>0.7</priority>
     <xhtml:link rel="alternate" hreflang="uk" href="https://endo.kh.ua/articles/${slug}"/>`;
 
-        // Check if RU translation exists
-        if (article.translations && article.translations.ru && article.translations.ru.title) {
+        if (hasRu) {
           dynamicUrls += `\n    <xhtml:link rel="alternate" hreflang="ru" href="https://endo.kh.ua/ru/articles/${slug}"/>`;
         }
 
         dynamicUrls += `\n  </url>`;
         
-        if (article.translations && article.translations.ru && article.translations.ru.title) {
+        if (hasRu) {
           dynamicUrls += `
   <url>
     <loc>https://endo.kh.ua/ru/articles/${slug}</loc>
@@ -47,13 +52,13 @@ function generateSitemap() {
     <xhtml:link rel="alternate" hreflang="ru" href="https://endo.kh.ua/ru/articles/${slug}"/>
   </url>`;
         }
-      }
-    });
+      });
+    }
 
     // Replace the closing tag with our dynamic URLs and the closing tag
     const finalSitemap = template.replace('</urlset>', dynamicUrls + '\n</urlset>');
     fs.writeFileSync(OUTPUT_PATH, finalSitemap);
-    console.log('[Sitemap] Automatically generated sitemap.xml with ' + articles.filter(a => a.status === 'published').length + ' articles.');
+    console.log('[Sitemap] Automatically generated sitemap.xml with ' + count + ' articles based on file system.');
 
   } catch (err) {
     console.error('[Sitemap Error] Failed to generate sitemap:', err.message);
